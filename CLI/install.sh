f3ce4509912387660f0088c8aaab960998d9de96
Plunne <plunne.celeste@gmail.com>
2023-04-18 13:57:40 +0200

    Scripts Linux tty

---
+++ CLI/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo cp monicom /usr/bin
+sudo cp morse_cli /usr/bin
--- CLI/monicom.sh
+++ CLI/monicom.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-DEV=/dev/ttyS7
-SPEED=9600
+DEV=/dev/$1
+
+if [ "$#" -eq 2 ] ; then
+   SPEED=$2
+else
+   SPEED=9600
+fi
+
+sudo chmod 777 $DEV
 
 stty -F $DEV speed $SPEED cs8
 while [ true ]; do
